11b891e6d97ab3873aede15955b2015b9d90e2c2
unknown <cristian.undurraga.l@gmail.com>
2022-11-13 18:02:02 -0300

    cambios en el renderizado, centrar el contenido, definir alturas

--- js/app.js
+++ js/app.js
@@ -1,45 +1,42 @@
-const xhr = new XMLHttpRequest();
-const targetHost = "http://localhost:3000/categorias";
-const urlBuscar = "http://localhost:3000/buscar/";
+const urlCategorias = "https://bsale-tiendaonline-entrevista.herokuapp.com/categorias";
+const urlBuscar = "https://bsale-tiendaonline-entrevista.herokuapp.com/buscar/";
 
-function crearMenuPorCategorias(){
-    xhr.onreadystatechange = function() {
-        if(this.readyState == 4 && this.status == 200){
-            const categorias = this.responseText
-            document.getElementById("menuCategorias").innerHTML = categorias;
+$( document ).ready(function() {
+    $.get( urlCategorias, function(response, status){
+        if (status === "success") {
+            const menuList = [];
+            response.map((menuItem) => {
+                menuList.push("<li><a id=" + menuItem.id + " class=dropdown-item href=# onclick=filtroCategorias(id) >" + menuItem.name + "</a></li>");
+            })
+            $("#menuCategorias").html(menuList.join(""));
+        } 
+    })
+});
+
+function filtroCategorias(id) {
+    $.get(urlCategorias + "/" + id, function(response, status){
+        if (status === "success") {
+            const listaProductos = [];
+            response.map((product) => {
+                listaProductos.push(
+                    "<div class='col-12 col-sm-3'><div class='card mb-5 customCard' style=width: 18rem;><img src=" + product.url_image + " class='card-img-top mt-5' alt=''><div class='card-body d-flex flex-column justify-content-center align-items-center'><h5 class=card-title>" + product.name + "</h5><p class=card-text>$" + product.price + "</p><a href=# class='btn btn-primary position-absolute bottom-0 mb-2'>Agregar al Carro</a></div></div></div>"
+                )
+            })
+            $("#productos").html(listaProductos.join(""));
         }
-    };
-
-    xhr.open("GET", targetHost, true)
-    xhr.send()
+    })
 }
 
-function buscarProductos(id) {
-
-    const targetUrlProductos = targetHost + "/" + id;
-
-    xhr.onreadystatechange = function() {
-        if(this.readyState == 4 && this.status == 200){
-            //const categorias = this.responseText
-            document.getElementById("productos").innerHTML = this.responseText;
+function buscarProductoNombre(){
+    $.get(urlBuscar + $("#formField").val(), function(response, status){
+        if (status === "success"){
+            const productoEncontrado = [];
+            response.map((product) => {
+                productoEncontrado.push(
+                    "<div class='col-12 col-sm-3'><div class='card mb-5 customCard' style=width: 18rem;><img src=" + product.url_image + " class='card-img-top mt-5' alt=''><div class='card-body d-flex flex-column justify-content-center align-items-center'><h5 class=card-title>" + product.name + "</h5><p class=card-text>$" + product.price + "</p><a href=# class='btn btn-primary position-absolute bottom-0 mb-2'>Agregar al Carro</a></div></div></div>"
+                )
+            })
+            $("#productos").html(productoEncontrado.join(""));
         }
-    };
-
-    xhr.open("GET", targetUrlProductos, true)
-    xhr.send()
-}
-
-function buscarProductoNombre() {
-    const campo = document.getElementById("formField").value;
-    const buscarProductoURL = urlBuscar + campo;
-
-    xhr.onreadystatechange = function() {
-        if(this.readyState == 4 && this.status == 200){
-            //const categorias = this.responseText
-            document.getElementById("productos").innerHTML = this.responseText;
-        }
-    };
-
-    xhr.open("GET", buscarProductoURL, true)
-    xhr.send()
+    })
 }
